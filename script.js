1fc0c164e17ed1d4e83be53ce2f43f0dfbd5988a
Mark Michael <mark.michael@vanderbilt.edu>
2016-05-15 11:02:14 -0500

    Incorporate responsive maps

--- script.js
+++ script.js
@@ -11,4 +11,29 @@ $(document).ready(function(){
     });
     $( "#amount" ).val( $( "#slider" ).slider( "value" )+" MONTHS" );
   });
+
+  $(".showmap1").click(function(){
+  	$(".showmap1").addClass("active");
+  	$(".showmap2").removeClass("active");
+  	$(".showmap3").removeClass("active");
+  	$("#map1").removeClass("hidden");
+  	$("#map2").addClass("hidden");
+  	$("#map3").addClass("hidden");
+  })
+  $(".showmap2").click(function(){
+  	$(".showmap2").addClass("active");
+  	$(".showmap1").removeClass("active");
+  	$(".showmap3").removeClass("active");
+  	$("#map2").removeClass("hidden");
+  	$("#map1").addClass("hidden");
+  	$("#map3").addClass("hidden");
+  })
+  $(".showmap3").click(function(){
+  	$(".showmap3").addClass("active");
+  	$(".showmap2").removeClass("active");
+  	$(".showmap1").removeClass("active");
+  	$("#map3").removeClass("hidden");
+  	$("#map2").addClass("hidden");
+  	$("#map1").addClass("hidden");
+  })
 });
